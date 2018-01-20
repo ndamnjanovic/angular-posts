9d100bee03ade7a6fd204c972cc6e367284afd6f
Nedeljko Damnjanovic <nedeljko.damnjanovic@gmail.com>
2018-01-20 11:42:24 +0100

    Added posts component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
+import { AppComponent } from "./app.component";
+import { LayoutComponent } from "./components/layout/layout.component";
+import { PostsComponent } from "./components/posts/posts.component";
 
-import { AppComponent } from './app.component';
-
+const appRoutes: Routes = [
+  {
+    path: "",
+    redirectTo: "/posts",
+    pathMatch: "full"
+  },
+  {
+    path: "posts",
+    component: PostsComponent
+  }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LayoutComponent, PostsComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
